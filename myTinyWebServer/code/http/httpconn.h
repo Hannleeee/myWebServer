8b8e8cb4543135a68609a0960e3a99b889c5fd6b
han <lh18801299790@gmail.com>
2021-10-03 18:39:14 +0800

    added http module

---
+++ myTinyWebServer/code/http/httpconn.h
@@ -0,0 +1,60 @@
+#ifndef HTTP_CONN_H
+#define HTTP_CONN_H
+
+#include <sys/types.h>
+#include <sys/uio.h>
+#include <arpa/inet.h>
+#include <stdio.h>
+#include <errno.h>
+
+#include "../log/log.h"
+#include "../pool/sqlconnRAII.h"
+#include "../buffer/buffer.h"
+#include "httprequest.h"
+#include "httpresponse.h"
+
+class HttpConn {
+public:
+    HttpConn();
+
+    ~HttpConn();
+
+    void init(int sockfd, const sockaddr_in &addr);
+
+    ssize_t read(int *saveErrno);
+
+    ssize_t write(int *saveErrno);
+
+    void Close();
+
+    int GetFd() const;
+    int GetPort() const;
+    const char *GetIP() const;
+    sockaddr_in GetAddr() const;
+
+    bool process();
+
+    int ToWriteBytes() {
+        return 
+    }
+
+    static bool isET;
+    static const char *srcDir;
+    static std::atomic<int> userCount;
+
+private:
+    int _fd;
+    struct sockaddr_in _addr;
+
+    bool _isClose;
+    int _iovCnt;
+    struct iovec _iov[2];
+
+    Buffer _readBuff;
+    Buffer _writeBuff;
+
+    HttpRequest _request;
+    HttpResponse _response;
+};
+
+#endif
