437fa98c2f59525d9b13d7ba147e9fe74fd4dcac
han <lh18801299790@gmail.com>
2021-10-10 23:14:31 +0800

    update

--- myTinyWebServer/code/server/epoller.cpp
+++ myTinyWebServer/code/server/epoller.cpp
@@ -30,6 +30,7 @@ bool Epoller::DelFd(int fd) {
     return epoll_ctl(_epollFd, EPOLL_CTL_DEL, fd, &ev) == 0;
 }
 
+// 返回发生事件的数量
 int Epoller::Wait(int timeoutMS) {
     return epoll_wait(_epollFd, &_events[0], static_cast<int>(_events.size()), timeoutMS);
 }
@@ -39,6 +40,7 @@ int Epoller::GetEventFd(size_t i) const {
     return _events[i].data.fd;
 }
 
+// 返回事件类型
 uint32_t Epoller::GetEvents(size_t i) const {
     assert(i < _events.size() && i >= 0);
     return _events[i].events;
